b5e1136acf69624ed0e30cad0dcfe3ce6ecfcb4f
Chris Van Patten <chris@vanpattenmedia.com>
2018-08-29 07:21:29 -0400

    Work on importing menu from WordPress

--- app/themes/tomodomo/functions.php
+++ app/themes/tomodomo/functions.php
@@ -2,28 +2,64 @@
 
 namespace Tomodomo\Theme;
 
+use Tomodomo\Models\Menu;
+
 /**
  * Add theme support
  *
  * @return void
  */
 function themeSupports() {
-	// Featured images
-	add_theme_support( 'post-thumbnails' );
+    // Featured images
+    add_theme_support('post-thumbnails');
 
-	// Gutenberg wide alignment
-	add_theme_support( 'align-wide' );
+    // Gutenberg wide alignment
+    add_theme_support('align-wide');
 
-	// Disable Gutenberg colors
-	add_theme_support( 'disable-custom-colors' );
+    // Disable Gutenberg colors
+    add_theme_support('disable-custom-colors');
 
-	// Auto-generate the title tag
-	add_theme_support( 'title-tag' );
+    // Auto-generate the title tag
+    add_theme_support('title-tag');
 
     return;
 }
 
 add_action('after_setup_theme', __NAMESPACE__ . '\\themeSupports');
+
+/**
+ * Register theme nav menus
+ *
+ * @return void
+ */
+function registerMenus()
+{
+    register_nav_menus([
+        'primary' => 'Primary Menu',
+        'footer'  => 'Footer Menu',
+    ]);
+
+    return;
+}
+
+add_action('init', __NAMESPACE__ . '\\registerMenus');
+
+/**
+ * Add items to Timber context to access on views
+ *
+ * @param array $context
+ *
+ * @return array
+ */
+function context($context) {
+    // Load in menus
+    $context['menu']['primary'] = new Menu('primary');
+    $context['menu']['footer']  = new Menu('footer');
+
+    return $context;
+}
+
+add_filter('timber/context', __NAMESPACE__ . '\\context');
 
 /*
 add_filter('Tomodomo\Kaiso\CustomTemplates', function (array $templates) {
