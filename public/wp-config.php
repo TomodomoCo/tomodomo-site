9f5fd2bc837282ef607b469b6b90a85575d335e8
Chris Van Patten <chris@vanpattenmedia.com>
2018-09-14 11:15:24 -0400

    Further simplify wp-config

--- public/wp-config.php
+++ public/wp-config.php
@@ -22,14 +22,12 @@ $database = $yaml->parse(file_get_contents("{$dir}/../config/secrets/database.ym
  */
 $failure_message = <<<EOT
 <html>
-<head>
-<title>Sorry!</title>
-</head>
-<body>
-<h1>Sorry!</h1>
-<p>We are having some trouble loading the site at the moment. Rest assured that the tech support teams have been dispatched and will be fixing this shortly!</p>
-<p>Please accept our apologies.</p>
-</body>
+    <head><title>Sorry!</title></head>
+    <body>
+        <h1>Sorry!</h1>
+        <p>We are having some trouble loading the site at the moment. Rest assured that the tech support teams have been dispatched and will be fixing this shortly!</p>
+        <p>Please accept our apologies.</p>
+    </body>
 </html>
 EOT;
 
@@ -37,78 +35,73 @@ EOT;
  * Sanity checking of the imported YAML.
  */
 if (
-	! is_array( $project ) ||
-	! is_array( $database ) ||
-	count( $project ) < 1 ||
-	count( $database ) < 1
-) {
+	!is_array($project) ||
+	!is_array($database) ||
+	count($project) < 1 ||
+    count($database) < 1 )
+{
 	header('HTTP/1.1 503 Service Unavailable');
+
 	echo $failure_message;
-	trigger_error( 'WPFRAME FATAL: === The project.yml and/or database.yml files could not be parsed for the ' . strip_tags( $_SERVER['SERVER_NAME'] ) . ' site.', E_USER_ERROR );
-	die();
+
+	trigger_error( 'WPFRAME FATAL: The project.yml and/or database.yml files could not be parsed for the ' . strip_tags( $_SERVER['SERVER_NAME'] ) . ' site.', E_USER_ERROR );
+
+	die;
 }
 
 /**
  * Setup the dev, staging, and production environments
  */
-if ( $_SERVER['SERVER_NAME'] === $project['stage']['dev']['domain'] ) {
+if ($_SERVER['SERVER_NAME'] === $project['stage']['dev']['domain']) {
 
 	/**
 	 * DEV
 	 */
-	define( 'WP_STAGE', 'dev' );
-	define( 'WP_HOME', 'http://' . $project['stage']['dev']['domain'] );
+	define('WP_STAGE', 'dev');
+	define('WP_HOME', "http://{$project['stage']['dev']['domain']}");
 
 	// Make "private"
-	define( 'WP_ROBOTS_PUBLIC', 0 );
+	define('WP_ROBOTS_PUBLIC', 0);
 
 	// Show errors
-	define( 'WP_DEBUG', true );
+	define('WP_DEBUG', true);
 
-} elseif ( $_SERVER['SERVER_NAME'] === $project['stage']['staging']['domain'] ) {
+} elseif ($_SERVER['SERVER_NAME'] === $project['stage']['staging']['domain']) {
 
 	/**
 	 * STAGING
 	 */
-	define( 'WP_STAGE', 'staging' );
-	define( 'WP_HOME', 'http://' . $project['stage']['staging']['domain'] );
+	define('WP_STAGE', 'staging');
+	define('WP_HOME', "http://{$project['stage']['staging']['domain']}");
 
 	// Make "private"
-	define( 'WP_ROBOTS_PUBLIC', 0 );
+	define('WP_ROBOTS_PUBLIC', 0);
 
 	// Hide errors
-	define( 'WP_DEBUG', false );
+	define('WP_DEBUG', false);
 
 } else {
 
 	/**
 	 * PRODUCTION
 	 */
-	define( 'WP_STAGE', 'production' );
-	define( 'WP_HOME', 'http://' . $project['stage'][WP_STAGE]['domain'] );
+	define('WP_STAGE', 'production');
+	define('WP_HOME', "https://{$project['stage'][WP_STAGE]['domain']}");
 
 	// Make public
-	define( 'WP_ROBOTS_PUBLIC', 1 );
+	define('WP_ROBOTS_PUBLIC', 1);
 
 	// Hide errors
-	define( 'WP_DEBUG', false );
+	define('WP_DEBUG', false);
 
 }
 
 /**
  * Misc. Settings
  */
-define( 'WP_POST_REVISIONS', 8 );
-
-/**
- * Language (leave blank for American English)
- */
-define( 'WPLANG', '' );
-
-/**
- * Path to WordPress
- */
-define( 'WP_SITEURL', WP_HOME );
+define('WP_POST_REVISIONS', 8); // Post revisions to store
+define('WPLANG', '');           // Language (leave blank for American English)
+define('WP_SITEURL', WP_HOME);  // Base URL for wp-admin
 
 /**
  * Custom Content Directory
@@ -119,34 +112,38 @@ define('WP_CONTENT_URL', WP_HOME . '/content');
 /**
  * Debug settings
  */
-if ( WP_DEBUG === true ) {
-	ini_set( 'display_errors', '1' );
-	define( 'WP_DEBUG_DISPLAY', true );
+if (WP_DEBUG === true) {
+	ini_set('display_errors', '1');
+	define('WP_DEBUG_DISPLAY', true);
 } else {
-	ini_set( 'display_errors', '0' );
-	define( 'WP_DEBUG_DISPLAY', false );
+	ini_set('display_errors', '0');
+	define('WP_DEBUG_DISPLAY', false);
 }
 
 /**
  * Set up databases
  */
-if ( array_key_exists( WP_STAGE, $database ) ) {
-	foreach ( $database[ WP_STAGE ] as $db_variable => $value ) {
-		define( ( 'DB_' . strtoupper( $db_variable ) ), $value );
+if (array_key_exists(WP_STAGE, $database)) {
+	foreach ($database[WP_STAGE] as $db_variable => $value) {
+		define(('DB_' . strtoupper($db_variable)), $value);
 	}
+
 	$table_prefix = DB_TBL_PREFIX;
 } else {
 	header('HTTP/1.1 503 Service Unavailable');
+
 	echo $failure_message;
-	trigger_error( 'WPFRAME FATAL: === The database.yml file does not have a \'' . strip_tags( WP_STAGE ) . '\' stage, so the DB details cannot be loaded for the ' . strip_tags( $_SERVER['SERVER_NAME'] ) . ' site.', E_USER_ERROR );
-	die();
+
+	trigger_error('WPFRAME FATAL: The database.yml file does not have a `' . strip_tags(WP_STAGE) . '` stage, so the DB details cannot be loaded for the `' . strip_tags($_SERVER['SERVER_NAME']) . '` site.', E_USER_ERROR);
+
+	die;
 }
 
 /**
  * You almost certainly do not want to change these
  */
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
 
 /**
  * Salts, for security
@@ -164,10 +161,4 @@ if (!defined('ABSPATH')) {
 	define('ABSPATH', "{$dir}/wp/");
 }
 
-require_once( ABSPATH . 'wp-settings.php' );
-
-/**
- * Set 'blog_public'
- */
-if ( defined( 'WP_ROBOTS_PUBLIC' ) && is_blog_installed() )
-	update_option( 'blog_public', WP_ROBOTS_PUBLIC );
+require_once(ABSPATH . 'wp-settings.php');
